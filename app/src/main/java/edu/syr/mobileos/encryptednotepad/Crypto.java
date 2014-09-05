4f5e91f190f5e2e4cf79cf5bcc83afa267a51a85
b7ack42 <b7ack42@gmail.com>
2014-09-04 21:53:18 -0400

    some update in crypto

--- app/src/main/java/edu/syr/mobileos/encryptednotepad/Crypto.java
+++ app/src/main/java/edu/syr/mobileos/encryptednotepad/Crypto.java
@@ -6,28 +6,38 @@ import junit.framework.Assert;
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import java.util.Random;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import javax.crypto.Mac;
+
 /**
  * Crypto library to support SHA256 and AES256
  */
 public class Crypto {
 
-    private static final String sIV = "0000000000000000"; //todo
-
     private static final String ENCODER = "ISO-8859-1";
 
+    //there might be something wrong here about sIV!!!
+    private static final byte[] sIV = new byte[256];// = "0000000000000000"; //todo
+    static{
+        new Random().nextBytes(sIV);
+        //sIV = "234";
+    }
     /**
      * Performs SHA256 on a string
      * @param s     The string to hash
      * @return      A byte array containing the resulting 32-byte hash
      */
+
     public static byte[] sha256(String s) {
         MessageDigest digest = null;
 
@@ -69,7 +79,7 @@ public class Crypto {
 
         if (cipher != null) {
             try {
-                cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(sIV.getBytes(ENCODER)));
+                cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(sIV));//.getBytes(ENCODER)));
                 byte[] encrypted_data = cipher.doFinal(data.getBytes(ENCODER));
                 return bin2String(encrypted_data);
             }
@@ -105,7 +115,7 @@ public class Crypto {
 
         if (cipher != null) {
             try {
-                cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(sIV.getBytes(ENCODER)));
+                cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(sIV));//.getBytes(ENCODER)));
                 byte [] decrypted_data = cipher.doFinal(data.getBytes(ENCODER));
                 return bin2String(decrypted_data);
             }
@@ -116,6 +126,39 @@ public class Crypto {
 
         return null;
     }
+
+
+    /**
+     * HMAC-SHA256 encoding
+     * @param key the 32-byte key to be HMAC
+     * @param data the random string to HMAC the key
+     * @return
+     */
+    public static String hmac_sha256(byte[] key, byte[] data){
+        Assert.assertEquals(32, key.length); // 32 bytes = 256-bit key
+        Mac mac = null;
+        try {
+            mac = Mac.getInstance("HmacSHA256");
+
+        }
+        catch (NoSuchAlgorithmException e){
+            Log.d("debug",e.toString());
+        }
+        if (mac != null){
+            try{
+                mac.init(new SecretKeySpec(key, "HmacSHA256"));
+                byte[] digest = mac.doFinal(data);
+                return bin2String(digest);
+
+
+            }
+            catch (Exception e){
+                Log.d("debug", e.toString());
+            }
+        }
+    return null;
+    }
+
 
     // useful for debugging
     public static String bin2hex(byte[] data) {
@@ -134,4 +177,12 @@ public class Crypto {
 
         return encoded_data;
     }
+
+    //for debugging
+
+    public byte[] get_sIV(){
+        return sIV;
+    }
 }
+
+
