6b98c529825b015add5695a54ae72b0f319fb50d
Scott Constable <fidget324@gmail.com>
2014-09-16 08:52:35 -0400

    Removed the test funtions

--- app/src/main/java/edu/syr/mobileos/encryptednotepad/MainActivity.java
+++ app/src/main/java/edu/syr/mobileos/encryptednotepad/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends Activity implements
@@ -36,7 +35,6 @@ public class MainActivity extends Activity implements
         for (int i = 0; i < mKey.length; i++)
             mKey[i] = 0;
         mENDBManagerObject.closeDatabase();
-        TestNotes.clear();
         finish();
     }
 
@@ -47,8 +45,6 @@ public class MainActivity extends Activity implements
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
-
-        new TestNotes();
 
         mENDBManagerObject = new ENDBManager(this);
         mENDBManagerObject.openDatabase();
@@ -141,17 +137,6 @@ public class MainActivity extends Activity implements
     @Override
     public void onFinishEditDialog(String password) {
         mKey = Crypto.sha256(password);
-
-        List<Note> test_notes = TestNotes.get();
-        Log.d("debug", test_notes.get(0).getText());
-        Log.d("debug", test_notes.get(1).getText());
-        Log.d("debug", test_notes.get(2).getText());
-
-        /* uncomment to make more notes */
-        mENDBManagerObject.addNote(encryptNote(test_notes.get(0)));
-        mENDBManagerObject.addNote(encryptNote(test_notes.get(1)));
-        mENDBManagerObject.addNote(encryptNote(test_notes.get(2)));
-
         updateNotes();
 
         getFragmentManager().beginTransaction()
@@ -232,29 +217,5 @@ public class MainActivity extends Activity implements
             if (note != null)
                 mNotes.add(note);
         }
-    }
-
-    // test function, please ignore
-    private void testCrypto() {
-        // Example demonstrating the Crypto class
-        byte[] key = mKey;
-        Log.d("debug", "key: " + StringUtility.bin2hex(key));
-        String plaintext = "123456789101112131415161718192021222324252627282930313233343536";
-
-        byte[] sIV = Crypto.getIV();
-        Log.d("debug", "IV: " + sIV);
-
-        Log.d("debug", "plaintext: " + plaintext);
-        String ciphertext = Crypto.aes256_enc(key, plaintext,sIV);
-        Log.d("debug", "ciphertext: " + ciphertext);
-
-        String plaintext2 = Crypto.aes256_dec(key, ciphertext,sIV);
-        Log.d("debug", "plaintext2: " + plaintext2);
-
-        String hmacText = Crypto.hmac_sha256(key, ciphertext);
-        Log.d("debug", "hmacText: "+hmacText);
-        //Log.d("debug",Crypto)
-
-        // Log.d("debug",Crypto.hm)
     }
 }
